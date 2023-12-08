6c6d48a9b6f08ed5fcb1ad02a00511fa2f483153
Tristan <tristan.cuvelier@student.hogent.be>
2023-12-08 15:01:10 +0100

    Removed unused code.

--- foxyBot.py
+++ foxyBot.py
@@ -24,31 +24,14 @@ async def ping(ctx):
 # Respond to a role mention
 @bot.event
 async def on_message(message):
-    # Ignore messages sent by the bot itself
     if message.author != bot.user:
     
     # Check if the message mentions a specific role
-    # Replace 'YOUR_ROLE_ID_HERE' with the actual role ID you want to check for
         bump_role = get(message.guild.roles, name="Bump Remind Ping")
-        if bump_role and bump_role.mention in message.content:
-            # The bot will type out '!bump' which should be picked up by the bump bot if it's set to listen for commands in messages
-            # await message.channel.send('/bump')
+        if bump_role and bump_role.mention in message.content:           
             execCommand.send_command()
-            # os.system('python3 executeSlashCommands.py')
-        
             # This is necessary to process commands if you override the on_message event
             await bot.process_commands(message)
-
-
-# @bot.command(name='bumpTest')
-# async def bump_test_command(ctx):
-#     # Call the function to execute the slash command from the command
-#     execCommand.send_command()
-#     # os.system('python3 executeSlashCommands.py')
-#     await ctx.send('Bump test command executed!')
-
-
-
 
 # Event listener for when the bot has connected to the server
 @bot.event
